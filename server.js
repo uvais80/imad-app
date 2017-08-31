1da8e20bff9419de901c52a081ffcb13b65f7f80
uvais80 <30869260+uvais80@users.noreply.github.com>
2017-08-31 18:04:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/Article_one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Article_one.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
